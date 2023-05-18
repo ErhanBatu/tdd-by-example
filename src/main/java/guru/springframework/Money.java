2ead6fff3cd3f3040590b1347240f042b78a216f
Erhan <e.batu44@icloud.com>
2023-05-18 11:15:27 +0200

    25. Times We're Living in

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -5,15 +5,24 @@ public abstract class Money {
     //This is my Base class including common properties in Franc and Dollar classes
 
     protected int amount;
+    protected String currency;
 
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected String currency(){
+        return currency;
+    }
     public abstract Money times (int multiplier);
 
     public static Money dollar(int amount){
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     public static Money franc(int amount){
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     public boolean equals(Object object){
