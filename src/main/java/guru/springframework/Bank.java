3042cc2c81ba496e1db2b0dfaa3896f934a002b3
Erhan <e.batu44@icloud.com>
2023-05-20 17:28:25 +0200

    30. Change

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -1,9 +1,28 @@
 package guru.springframework;
 
+import java.util.HashMap;
+
 public class Bank {
 
+    private HashMap<Pair, Integer> rateMap = new HashMap<>();
+
     Money reduce(Expression source, String toCurency){
-        return source.reduce(toCurency);
+        return source.reduce(this, toCurency);
+    }
+
+    public int rate(String from, String to){
+        if(from.equals(to)){
+            return 1;
+        }
+
+        return rateMap.get(new Pair(from, to));
+    }
+
+    public void addRate(String from, String to, int rate) {
+
+        rateMap.put(new Pair(from, to), rate);
+
+
     }
 }
 
